5c11be9f39ce169110d437ae69cc6eb1cad23229
taufiqjack <taufiqdwic@gmail.com>
2021-03-12 22:32:24 +0700

    bug fixing firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:angsoduo_pelaporanmasyarakat/pages/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
@@ -10,9 +13,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Firebase.initializeApp();
-    return MaterialApp(
+    return new MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
