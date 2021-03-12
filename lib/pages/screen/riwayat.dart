07b289b5666f1c90659d48ee5b257d0d6ac9a9a8
taufiqjack <taufiqdwic@gmail.com>
2021-03-12 10:23:03 +0700

    adding dashboard page and riwayat laporan pages

--- lib/pages/screen/riwayat.dart
+++ lib/pages/screen/riwayat.dart
@@ -11,39 +11,41 @@ class RiwayatLaporan extends StatefulWidget {
 class _RiwayatLaporanState extends State<RiwayatLaporan> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Center(
-          child: Column(
-            children: [
-              Text(
-                'RIWAYAT LAPORAN',
-                style: GoogleFonts.inter(
-                    color: Colors.black, fontWeight: FontWeight.w900),
-              ),
-              Padding(padding: EdgeInsets.all(10)),
-            ],
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Center(
+            child: Column(
+              children: [
+                Text(
+                  'RIWAYAT LAPORAN',
+                  style: GoogleFonts.inter(
+                      color: Colors.black, fontWeight: FontWeight.w900),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+              ],
+            ),
           ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(20, 24, 24, 8),
-          child: Card(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ListTile(
-                  leading: new Text(
-                    'coba',
-                    style: GoogleFonts.inter(
-                      color: Colors.black,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(20, 24, 24, 8),
+            child: Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: new Text(
+                      'coba',
+                      style: GoogleFonts.inter(
+                        color: Colors.black,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
