3f2a857b47237122ff073845025739ce7a6cc173
taufiqjack <taufiqdwic@gmail.com>
2021-03-15 14:25:39 +0700

    change status details list

--- lib/pages/screen/status.dart
+++ lib/pages/screen/status.dart
@@ -16,45 +16,48 @@ class _StatusLaporanState extends State<StatusLaporan> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(20.0, 24.0, 24.0, 8.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/images/group24.png',
-                    height: 100,
-                    width: 100,
-                    fit: BoxFit.fill,
-                  ),
-                  Padding(padding: EdgeInsets.all(10)),
-                  Text(
-                    'LAPORAN SUKSES TERKIRIM',
-                    style: GoogleFonts.inter(
-                        fontSize: 18, fontWeight: FontWeight.w800),
-                  ),
-                  Padding(padding: EdgeInsets.all(10)),
-                  Text(
-                    'Terima kasih atas laporan anda. Command center',
-                    style: GoogleFonts.inter(
-                        fontSize: 13, fontWeight: FontWeight.w400),
-                  ),
-                  Text(
-                    'polda Jambi akan menindaklanjuti laporan anda',
-                    style: GoogleFonts.inter(
-                        fontSize: 13, fontWeight: FontWeight.w400),
-                  ),
-                  Text(
-                    'sesegera mungkin.',
-                    style: GoogleFonts.inter(
-                        fontSize: 13, fontWeight: FontWeight.w400),
-                  )
-                ],
+      body: WillPopScope(
+        onWillPop: () => Future.value(false),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(20.0, 24.0, 24.0, 8.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/group24.png',
+                      height: 100,
+                      width: 100,
+                      fit: BoxFit.fill,
+                    ),
+                    Padding(padding: EdgeInsets.all(10)),
+                    Text(
+                      'LAPORAN SUKSES TERKIRIM',
+                      style: GoogleFonts.inter(
+                          fontSize: 18, fontWeight: FontWeight.w800),
+                    ),
+                    Padding(padding: EdgeInsets.all(10)),
+                    Text(
+                      'Terima kasih atas laporan anda. Command center',
+                      style: GoogleFonts.inter(
+                          fontSize: 13, fontWeight: FontWeight.w400),
+                    ),
+                    Text(
+                      'polda Jambi akan menindaklanjuti laporan anda',
+                      style: GoogleFonts.inter(
+                          fontSize: 13, fontWeight: FontWeight.w400),
+                    ),
+                    Text(
+                      'sesegera mungkin.',
+                      style: GoogleFonts.inter(
+                          fontSize: 13, fontWeight: FontWeight.w400),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
