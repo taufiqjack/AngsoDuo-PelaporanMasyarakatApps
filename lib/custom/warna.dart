58bf22f9ae9c4c80ae2058a090d0b6549bb15bc3
taufiqjack <taufiqdwic@gmail.com>
2021-03-15 14:26:04 +0700

    adding colors custom

--- lib/custom/warna.dart
+++ lib/custom/warna.dart
@@ -4,4 +4,5 @@ class Warna {
   static const solidblue = const Color(0xff514CD9);
   static const white = const Color(0xffFFFFFF);
   static const warmpink = const Color(0xffEF5A5C);
+  static const greysolid = const Color(0xffF7F6FF);
 }
